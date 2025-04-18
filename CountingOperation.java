471feb2b817deb7439828e8a760f7fe974173de9
Anusri Kadam <anusrikadam@gmail.com>
2025-04-19 00:32:47 +0530

    Update CountingOperation.java

--- CountingOperation.java
+++ CountingOperation.java
@@ -1,2 +1,15 @@
+// CountingOperation.java
 public class CountingOperation implements Runnable {
+
+    // Thread logic to count from 1 to 5
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Counting: " + i);
+            try {
+                Thread.sleep(400); // Pause for 400ms
+            } catch (InterruptedException e) {
+                System.out.println("CountingOperation interrupted.");
+            }
+        }
+    }
 }
